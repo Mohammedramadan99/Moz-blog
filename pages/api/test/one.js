be0121947ba426377dc2dcddb4ae864a758d32ac
mohammed ramadan <ramadanmohammed502@gmail.com>
2023-02-13 15:54:48 +0200

    test api post

--- pages/api/test/one.js
+++ pages/api/test/one.js
@@ -6,7 +6,7 @@ const handler = nc();
 
 handler.get(async (req, res) =>
 {
-  await db.connect();
+  // await db.connect();
   try
   {
     // const posts = await Post.find({}).populate('author').sort({createdAt:-1})
@@ -21,7 +21,7 @@ handler.get(async (req, res) =>
       message: err.message,
     });
   }
-  await db.disconnect();
+  // await db.disconnect();
 })
 
 
